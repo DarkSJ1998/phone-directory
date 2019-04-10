f8da3530fd26a687e2b2579180c89d5e132a84f8
DarkSJ1998 <sarthakjn12@gmail.com>
2019-04-10 15:58:09 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <div className="header">
+          Phone Directory
+        </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span><br/>
+          <span> Phone </span>
+        </div>
       </div>
     );
   }
